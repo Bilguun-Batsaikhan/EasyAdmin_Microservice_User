6c0b9dae5236157d37135d9dffa3739e0331b6ef
DESKTOP-9D2J144\certimeter <bilguun.batsaikhan@lutech.it>
2024-12-05 14:35:10 +0100

    just removing some comments

--- src/main/java/com/certimetergroup/myapp/exception/UserExceptionController.java
+++ src/main/java/com/certimetergroup/myapp/exception/UserExceptionController.java
@@ -10,18 +10,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 //This annotation is used to handle exceptions globally across all controllers in a Spring application. It allows to consolidate my exception handling logic in one place.
 @RestControllerAdvice
 public class UserExceptionController {
-    //This annotation is used to specify which exception this method will handle.
-//    @ExceptionHandler(value = UserNotFoundException.class)
-//    public ResponseEntity<Object> exception(UserNotFoundException exception) {
-//        //This line creates a new ResponseEntity with a message “User not found” and an HTTP status code of NOT_FOUND (404). This response is sent back to the client when the exception occurs.
-//        return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
-//    }
-//
-//    @ExceptionHandler(value = UserAlreadyExistsException.class)
-//    public ResponseEntity<Object> exception(UserAlreadyExistsException exception) {
-//        return new ResponseEntity<>("User already exists", HttpStatus.CONFLICT);
-//    }
-
     @ExceptionHandler(FailureException.class)
     public ResponseEntity<Response> handleFailureException(FailureException exception) {
         ResponseEnum responseEnum = exception.getResponseEnum();
